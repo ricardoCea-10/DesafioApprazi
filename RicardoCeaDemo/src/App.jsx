b3d7942d5364fe7c4112e2b3a68b682332e604ce
ricardoCea-10 <ricardocea.dev@gmail.com>
2025-02-18 01:03:43 -0300

    instalo componentes basicos de Ionic

--- RicardoCeaDemo/src/App.jsx
+++ RicardoCeaDemo/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { setupIonicReact } from '@ionic/react';
+
+// funcion para disponibilizar componentes
+setupIonicReact();
 
 function App() {
   const [count, setCount] = useState(0)
--- RicardoCeaDemo/src/main.jsx
+++ RicardoCeaDemo/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@ionic/react/css/core.css';
 import App from './App.jsx'
-import app from './firebase-config.js';
-
+// Importa Firebase desde archivo config
+import app from './firebase/firebase-config.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
