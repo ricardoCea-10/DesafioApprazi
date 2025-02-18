ac194ebac419f5f7e44911c78b43f47a8559e6a2
ricardoCea-10 <ricardocea.dev@gmail.com>
2025-02-18 00:06:28 -0300

    anado firebase a proyecto react vite

--- RicardoCeaDemo/src/firebase-config.js
+++ RicardoCeaDemo/src/firebase-config.js
@@ -18,7 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 // export const auth = getAuth(app);
+
 // Analíticas
 const analytics = getAnalytics(app);
 
